767e3259885bd8f850b6d7c7fdac881734231cab
JustinMiroux <justin@miroux.eu>
2025-02-21 13:24:46 +0100

    fin labo 1

--- Q2-Programmation-OS/Bash/labo1.sh
+++ Q2-Programmation-OS/Bash/labo1.sh
@@ -2,5 +2,13 @@
 
 interne="Programmation"
 
-echo $interne
-echo $publique
+[ -z "${publique+x}" ] && echo "La variable publique n'est pas affeectée. Exiting." >&2 && exit 1
+
+if [ -n "$publique" ]; then
+    echo "${interne}${publique}"
+else
+    echo "La variable publique est vide" >&2
+    exit 2
+fi
+
+exit 0
