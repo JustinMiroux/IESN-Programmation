ddc28097fc350a232717d3da7376b4c07709932d
JustinMiroux <justin@miroux.eu>
2025-03-12 09:38:50 +0100

    Labo 3 Executor completed

--- Q2-Programmation/Labo3/Executor/executorlab3server.py
+++ Q2-Programmation/Labo3/Executor/executorlab3server.py
@@ -1,4 +1,4 @@
-import threading
+import concurrent.futures
 import time
 from socket import socket, AF_INET, SOCK_STREAM
 
@@ -6,10 +6,14 @@ def serve(domain, port):
     with socket(AF_INET, SOCK_STREAM) as server_socket:
         server_socket.bind((domain, port))
         server_socket.listen()
+        executor = concurrent.futures.ThreadPoolExecutor(max_workers=100)
         while True:
             client_socket, client_address = server_socket.accept()
             print(f"Connection from {client_address}")
-            threading.Thread(target=handle_request, args=[client_socket]).start()
+
+            #threading.Thread(target=handle_request, args=[client_socket]).start()
+            executor.submit(handle_request, client_socket)
+        
 
 def handle_request(client_socket: socket):
     request = client_socket.recv(1024).decode()
